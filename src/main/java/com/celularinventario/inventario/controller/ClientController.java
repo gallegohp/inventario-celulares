ad87594963c99b9cef2549f090de6400a5c8151f
rixhard-rxp <oskitarmolpa@gmail.com>
2026-03-10 10:46:05 -0500

    feat: creacion de Json(PhoneResponse)

---
+++ src/main/java/com/celularinventario/inventario/controller/ClientController.java
@@ -0,0 +1,33 @@
+package com.celularinventario.inventario.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.celularinventario.inventario.dto.PhoneResponseDTO;
+import com.celularinventario.inventario.services.ClientServices;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/client")
+@RestController
+public class ClientController {
+    
+    private final ClientServices clientServices;
+
+    @PatchMapping("/buy/{id}/{quantity}")
+    public ResponseEntity<String> buyPhone(@PathVariable Integer id, @PathVariable Integer quantity) {
+        try {
+            PhoneResponseDTO response = clientServices.buyPhone(id, quantity);
+            return ResponseEntity.ok(response.getMessage());
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error: " + e.getMessage());
+        }
+    }
+
+
+}
---
+++ src/main/java/com/celularinventario/inventario/services/ClientServices.java
@@ -0,0 +1,36 @@
+package com.celularinventario.inventario.services;
+
+import org.springframework.stereotype.Service;
+
+import com.celularinventario.inventario.dto.PhoneResponseDTO;
+import com.celularinventario.inventario.entity.Phone;
+import com.celularinventario.inventario.repository.PhoneRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ClientServices {
+    
+    private final PhoneRepository phoneRepository;
+
+    public PhoneResponseDTO buyPhone(Integer id, Integer quantity) {
+
+        Phone phone = phoneRepository.findById(id).orElseThrow(() -> new RuntimeException("Celular no encontrado"));
+
+        if (phone.getStock() < quantity) {
+            
+            throw new RuntimeException("No hay suficiente stock disponible");
+        }
+
+        phone.setStock(phone.getStock() - quantity);
+        if (phone.getStock() == 0) {
+            phone.setDisponibilidad(false);
+        }
+        phoneRepository.save(phone);
+        PhoneResponseDTO responseDTO = new PhoneResponseDTO();
+        responseDTO.setMessage("Compra exitosa");
+        return responseDTO;
+    } 
+
+}
