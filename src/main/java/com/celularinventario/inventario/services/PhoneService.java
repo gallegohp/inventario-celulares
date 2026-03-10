83d38703f9c3f3b90c7f4e34fcb9aff901485497
rixhard-rxp <oskitarmolpa@gmail.com>
2026-03-10 18:18:26 -0500

    fix: Mensajes de Confirmación Refinados

--- src/main/java/com/celularinventario/inventario/services/PhoneService.java
+++ src/main/java/com/celularinventario/inventario/services/PhoneService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.celularinventario.inventario.dto.PhoneRequestDTO;
 import com.celularinventario.inventario.dto.PhoneResponseDTO;
@@ -17,8 +18,10 @@ import lombok.RequiredArgsConstructor;
 public class PhoneService {
 
     private final PhoneRepository phoneRepository;
+
     /**
      * Crea un nuevo phone.
+     * 
      * @param usersRequestDTO Objeto con los datos de entrada
      * 
      * @return MessageResponseDTO confirma la creación exitosaa
@@ -27,19 +30,18 @@ public class PhoneService {
         Phone phone = new Phone();
         phone.setMarca(phoneRequestDTO.getMarca());
         phone.setModelo(phoneRequestDTO.getModelo());
-        phone.setAnio(phoneRequestDTO.getAnio());     
-        phone.setStock(phoneRequestDTO.getStock());   
-        phone.setPrecio(phoneRequestDTO.getPrecio()); 
+        phone.setAnio(phoneRequestDTO.getAnio());
+        phone.setStock(phoneRequestDTO.getStock());
+        phone.setPrecio(phoneRequestDTO.getPrecio());
         phone.setDisponibilidad(phoneRequestDTO.getDisponibilidad());
 
         phoneRepository.save(phone);
 
         PhoneResponseDTO response = new PhoneResponseDTO();
-        response.setMessage("Celular registrado correctamente");
+        response.setMessage("Celular Registrado Correctamente");
 
         return response;
     }
-
 
     /**
      * * @return
@@ -54,9 +56,9 @@ public class PhoneService {
             phoneResponseDTO.setId(Aphone.getId());
             phoneResponseDTO.setMarca(Aphone.getMarca());
             phoneResponseDTO.setModelo(Aphone.getModelo());
-            phoneResponseDTO.setAnio(Aphone.getAnio());     
-            phoneResponseDTO.setStock(Aphone.getStock());   
-            phoneResponseDTO.setPrecio(Aphone.getPrecio()); 
+            phoneResponseDTO.setAnio(Aphone.getAnio());
+            phoneResponseDTO.setStock(Aphone.getStock());
+            phoneResponseDTO.setPrecio(Aphone.getPrecio());
             phoneResponseDTO.setDisponibilidad(Aphone.getDisponibilidad());
 
             listPhones.add(phoneResponseDTO);
@@ -64,11 +66,10 @@ public class PhoneService {
         return listPhones;
     }
 
-
     /**
-     * * @param id Identificador único 
+     * * @param id Identificador único
      * 
-     * @return 
+     * @return
      */
     public Optional<PhoneResponseDTO> getAPhone(Integer id) {
         Optional<Phone> optionalPhone = phoneRepository.findById(id);
@@ -80,9 +81,9 @@ public class PhoneService {
             response.setId(phone.getId());
             response.setMarca(phone.getMarca());
             response.setModelo(phone.getModelo());
-            response.setAnio(phone.getAnio());     
-            response.setStock(phone.getStock());   
-            response.setPrecio(phone.getPrecio()); 
+            response.setAnio(phone.getAnio());
+            response.setStock(phone.getStock());
+            response.setPrecio(phone.getPrecio());
             response.setDisponibilidad(phone.getDisponibilidad());
 
             return Optional.of(response);
@@ -91,10 +92,14 @@ public class PhoneService {
         }
     }
 
+    public PhoneResponseDTO deletePhone(Integer id) {
 
-    public void deletePhone(Integer id) {
         if (phoneRepository.existsById(id)) {
             phoneRepository.deleteById(id);
         }
+        PhoneResponseDTO response = new PhoneResponseDTO();
+        response.setMessage("Celular Eliminado Correctamente");
+
+        return response;
     }
 }
