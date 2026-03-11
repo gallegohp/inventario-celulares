93641fc60ca7631e27636c9539d3f05bc298a6fe
Camilo Gallego <c.gallego3029@gmail.com>
2026-03-10 19:43:01 -0500

    feat: pruebas de consumo de api local

--- src/main/java/com/celularinventario/inventario/controller/ClientController.java
+++ src/main/java/com/celularinventario/inventario/controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.celularinventario.inventario.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import com.celularinventario.inventario.dto.PhoneResponseDTO;
 import com.celularinventario.inventario.services.ClientServices;
 
 import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "*")
 
 @RequiredArgsConstructor
 @RequestMapping("/client")
